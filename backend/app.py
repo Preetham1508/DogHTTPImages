a5cf61e83e0133a2995ac8de7df3df188696e56e
Preetham1508 <103275568+Preetham1508@users.noreply.github.com>
2025-05-13 21:49:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -228,4 +228,4 @@ def update_list(current_user, list_id):
         return jsonify({"error": str(e)}), 400
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
